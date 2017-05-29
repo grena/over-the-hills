a6ecfa5c1457dde2a0c07f4c84262bd8a35e3805
Adrien Pétremann <petremann.adrien@gmail.com>
2017-05-29 20:43:12 +0200

    Init project

---
+++ Assets/Scripts/CursorFollower.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorFollower : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, 1.0f);
+	}
+}
---
+++ Assets/Scripts/SheepAI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepAI : MonoBehaviour {
+
+    [Header("Movements")]
+    public float moveSpeed;
+    public GameObject target;
+    [Space(10)]
+
+    private Vector3 movementVector;
+
+    // Use this for initialization
+    void Start()
+    {
+        movementVector = new Vector3(moveSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 toTargetVector = (target.transform.position - transform.position);
+        Vector3 newAccelerationVector = toTargetVector.normalized * moveSpeed;
+
+        movementVector = (50 * movementVector + newAccelerationVector) / 51;
+
+        transform.position = transform.position + movementVector;
+    }
+}
