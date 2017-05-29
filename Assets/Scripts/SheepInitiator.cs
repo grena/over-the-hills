d677c7b61fd75f9a1178bb459e7bbfe8748af5bc
Adrien Pétremann <petremann.adrien@gmail.com>
2017-05-29 21:36:32 +0200

    Add collision between sheeps

---
+++ Assets/Scripts/SheepInitiator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepInitiator : MonoBehaviour {
+
+    [Header("Parameters")]
+    public int sheepNumber;
+
+    [Header("Collaborators")]
+    public GameObject sheepModel;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < sheepNumber; i++)
+        {
+            GameObject sheep = Instantiate(sheepModel);
+            sheep.GetComponent<SheepAI>().target = GameObject.FindGameObjectWithTag("Cursor");
+            sheep.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), 1);
+        }
+	}
+}
